68ffa3b6929c2045fa855a2db16ecbc296eb0474
Joseph Macaraig <jbmacaraig14@gmail.com>
2024-05-14 13:38:03 -0700

    finished files
    might implement details for pokemon

--- src/app/components/PokemonCard.tsx
+++ src/app/components/PokemonCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export interface PokemonProp {
   name: string;
@@ -11,17 +12,17 @@ interface Prop {
 
 export const PokemonCard = ({ pokemon }: Prop) => {
   return (
-    <div className="flex flex-col items-center py-12 text-black border rounded-lg font-medium w-60 px-2 hover:scale-110 duration-300 shadow-md cursor-pointer">
-      <div className="relative w-full h-[20vh] flex items-center justify-center">
-        <Image
-          src={`https://img.pokemondb.net/artwork/${pokemon?.name}.jpg`}
-          alt={`${pokemon?.name} photo`}
-          width={100}
-          height={100}
-          loading="lazy"
-        />
+      <div className="flex flex-col items-center py-12 text-black border rounded-lg font-medium w-60 px-2 hover:scale-110 duration-300 shadow-md cursor-pointer">
+        <div className="relative w-full h-[20vh] flex items-center justify-center">
+          <Image
+            src={`https://img.pokemondb.net/artwork/${pokemon?.name}.jpg`}
+            alt={`${pokemon?.name} photo`}
+            width={100}
+            height={100}
+            loading="lazy"
+          />
+        </div>
+        <h1 className="text-lg pt-4">{pokemon?.name}</h1>
       </div>
-      <h1 className="text-lg pt-4">{pokemon?.name}</h1>
-    </div>
   );
 };
